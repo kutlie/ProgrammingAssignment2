6c3b98aeecd9597c878361564218b946a17dff9f
kutlie <kutloanotsoananyana@outlook.com>
2020-08-17 12:14:46 +0200

    Adding functional code into the function templates

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,32 @@
 ## functions do
 
 ## Write a short comment describing this function
+## The function makeCacheMatrix() creates a special "vector", which is really a list containing functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  theInverse <- matrix()
+  set <- function(y){
+    x <<- y
+    theInverse <<- matrix()
+  }
+  getData <- function() x
+  getInverse <- function() theInverse
+  setInverse <- function(inverse) theInverse <<- inverse
+  list(set=set, get=getData, setInverse=setInverse, getInverse=getInverse)
 }
 
 
 ## Write a short comment describing this function
+## The function cacheSolve() calculates the inverse of a matrix if receiving data for the first time, otherwise it gets it if it has already been calculated
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  theInverse <- x$getInverse()
+  if(!is.na(theInverse)){
+    message("getting cached data")
+    return(theInverse)
+  }
+  data <- x$get()
+  theInverse <- solve(data,...)
+  x$setInverse(theInverse)
+  theInverse
 }
